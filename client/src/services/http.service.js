a713a863fe56e564fd5b9fce3ff18f557bf4339a
Olga Melnikova <omelnikova@accesssoftek.com>
2022-04-20 09:23:27 +0400

    corrected production

--- client/src/services/http.service.js
+++ client/src/services/http.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const http = axios.create({ baseURL: "http://app-166650d1-6ca6-4033-ba32-5a7cb09b651d.cleverapps.io:5000/" });
+const http = axios.create({ baseURL: "http://31.184.253.165:5000/" });
 // const http = axios.create({ baseURL: "http://localhost:5000/" });
 // const http = axios.create({ baseURL: "http://app-166650d1-6ca6-4033-ba32-5a7cb09b651d.cleverapps.io:5000/" });
 // const http = axios.create({ baseURL: "http://backendlink/" });
