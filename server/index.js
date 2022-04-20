e1072446388b97cb2861f74cded8d3826f88384e
Olga Melnikova <omelnikova@accesssoftek.com>
2022-04-20 11:29:27 +0400

    remove api users

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/api/posts", postRoutes);
-app.use("/api/user", userRoutes);
+app.use("/user", userRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("server is up");
