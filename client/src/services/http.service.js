0cda9d465eeb7f8de6e62040ceeebed30c132b7e
Olga Melnikova <omelnikova@accesssoftek.com>
2022-04-20 08:41:59 +0400

    port

---
+++ client/src/services/http.service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const http = axios.create({ baseURL: "http://31.184.253.165/" });
+// const http = axios.create({ baseURL: "http://backendlink/" });
+
+http.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
+  }
+  return req;
+});
+
+const httpService = {
+  get: http.get,
+  post: http.post,
+  put: http.put,
+  delete: http.delete,
+  patch: http.patch,
+};
+
+export default httpService;
