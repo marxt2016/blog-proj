87cf05ffbdfb5756d8e8a697433a8b163bb07e06
Olga Melnikova <omelnikova@accesssoftek.com>
2022-04-20 11:20:10 +0400

    api users

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/api/posts", postRoutes);
-app.use("/user", userRoutes);
+app.use("/api/user", userRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("server is up");
