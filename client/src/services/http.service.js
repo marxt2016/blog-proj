d06d4c9b6d06faaaf4f8c9337507649a83430fa4
Olga Melnikova <omelnikova@accesssoftek.com>
2022-04-20 09:44:57 +0400

    changed url

--- client/src/services/http.service.js
+++ client/src/services/http.service.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const http = axios.create({ baseURL: "http://31.184.253.165:5000/" });
+// const http = axios.create({ baseURL: "http://31.184.253.165:5000/" });
 // const http = axios.create({ baseURL: "http://localhost:5000/" });
-// const http = axios.create({ baseURL: "http://app-166650d1-6ca6-4033-ba32-5a7cb09b651d.cleverapps.io:5000/" });
+const http = axios.create({ baseURL: "http://app-166650d1-6ca6-4033-ba32-5a7cb09b651d.cleverapps.io:5000/" });
 // const http = axios.create({ baseURL: "http://backendlink/" });
 
 http.interceptors.request.use((req) => {
